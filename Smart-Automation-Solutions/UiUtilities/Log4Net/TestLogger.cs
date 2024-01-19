58a0fd52d384dd524351545bcf1cadc7bbc7dcbf
Johil Angelo <johil_angelo@epam.com>
2024-01-19 09:57:51 +0530

    [WIP] UI Utilities added

---
+++ Smart-Automation-Solutions/UiUtilities/Log4Net/TestLogger.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+
+namespace UiUtilities.Log4Net
+{
+    public class TestLogger<T>
+    {
+        public ILog TestLog4Net()
+        {
+            PatternLayout patternLayout = new()
+            {
+                ConversionPattern = "%date{ABSOLUTE} - %message  %newline"
+            };
+            patternLayout.ActivateOptions();
+
+            ConsoleAppender consoleAppender = new()
+            {
+                Name = "ConsoleAppender",
+                Layout = patternLayout,
+                Threshold = Level.All
+        };
+            var fileAppender = new FileAppender()
+            {
+                Name = "FileAppender",
+                Layout = patternLayout,
+                Threshold = Level.All,
+                AppendToFile = false,
+                File = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory)!.Parent!.Parent!.Parent!.FullName, "Tests\\Logs\\FileLogger.log")
+            };
+
+            fileAppender.ActivateOptions();
+            consoleAppender.ActivateOptions();
+            BasicConfigurator.Configure(consoleAppender);
+            return LogManager.GetLogger(typeof(T));
+            
+        }
+    }
+}
